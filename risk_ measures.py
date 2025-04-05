e30a9338dfc7eddcb8b31e3ee1bc74f3188fb28d
Hello-World123456789 <shalinisamaran@gmail.com>
2025-04-05 17:49:31 +0100

    Adding Pandas to Inflation Risk

--- risk_ measures.py
+++ risk_ measures.py
@@ -1,22 +1,24 @@
 import numpy
 import math
 import json
+import pandas as pd
 
-fobj = open("cpi_index_all_00-25.csv","r")
-cpiData = fobj.read()
-print (cpiData)
-cpiData = 
-fobj.close()
-  
+
+cpiData = pd.read_csv('cpi_index_all_00-25.csv', index_col='Year')
+
+
 END_VALUE = 10
 BEGINNING_VALUE = 100
 
 statement1 = "Grant Lopez is 47 years old and has a budget of $2716 per year. He started investing on June 16th, 2023 and ended on October 22nd, 2023. His hobbies are painting and he avoids Crypto Assets."
 
-T0 = 0
-T1 = 1
+T0_month = 'Jun'
+T0_year = 2023
+T1_month = 'Oct'
+T1_year = 2023
 
-def inflationRisk(END_VALUE, BEGINNING_VALUE, T0, T1, SALARY):
+
+def inflationRisk(END_VALUE, BEGINNING_VALUE, T0_month, T0_year, T1_month, T1_year):
     # The inflation rate at time 0 must be greater than the investment return
     # at time 1 for positive real returns
     
@@ -24,16 +26,10 @@ def inflationRisk(END_VALUE, BEGINNING_VALUE, T0, T1, SALARY):
     nominalStockReturn = (END_VALUE - BEGINNING_VALUE)/BEGINNING_VALUE
     
     # Consumer Price Index (CPI) data set to measure US stock inflationary risk
-    if SALARY < 40,000:
-        endCPI = 
-        beginningCPI =
+    endCPI = cpiData.loc[T1_year,T1_month]
+    beginningCPI =cpiData.loc[T0_year,T0_month]
 
-    else if:
-        endCPI =
-        beginningCPI =
-
-    else: 
-    
+##
     inflationRate = (endCPI - beginningCPI) / beginningCPI
     
     # Calculate real returns
@@ -45,3 +41,5 @@ def inflationRisk(END_VALUE, BEGINNING_VALUE, T0, T1, SALARY):
         return 1
     else:
         return 0
+
+print(inflationRisk(500,499, T0_month, T0_year, T1_month, T1_year))
